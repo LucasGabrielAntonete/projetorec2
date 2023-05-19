87346b3e28615cf27a98dfc91c723b6edfab6f6f
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-05-19 11:58:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+
+ 
+
+</script>
+
+<template>
+  
+<div class="listagem-livros">
+  <p>livros</p>
+  <div class="card-livros" v-for="livro in livros" :key="livro.id">
+    <p>{{ livro.titulo }}</p>
+    <p>{{ livro.autor }}</p>
+    <p>{{ livro.preco }}</p>
+  
+  </div>
+
+</div>
+
+</template>
---
+++ src/_data/livros.js
@@ -0,0 +1,51 @@
+export const livros = [
+    {
+      id:1,
+      titulo: 'livro1',
+      autor: 'autor1',
+      preco: 50.00,
+  
+    },
+  
+    {
+      id:2,
+      titulo: 'livro2',
+      autor: 'autor2',
+      preco: 30.00,
+  
+    },
+  
+    {
+      id:3,
+      titulo: 'livro3',
+      autor: 'autor3',
+      preco: 60.00,
+  
+    },
+    {
+  
+      id:4,
+      titulo: 'livro4',
+      autor: 'autor4',
+      preco: 20.00,
+  
+    
+    },
+  
+    {
+      id:5,
+      titulo: 'livro5',
+      autor: 'autor5',
+      preco: 10.00,
+  
+    },
+    {
+      id:6,
+      titulo: 'livro6',
+      autor: 'autor6',
+      preco: 90.00,
+  
+    }
+  ]
+
+ 
