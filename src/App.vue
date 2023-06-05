1f936291baa628aa3da52809ec71bdd2e33977b6
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-06-05 10:58:00 -0300

    Arrumar o erro na divisão de telas e terminar o form

--- src/App.vue
+++ src/App.vue
@@ -151,105 +151,125 @@ const enviarForm = ref(false)
     </div>
 
     <div
+      class="absolute min-h-screen w-full bg-black z-20 opacity-60 overflow-hidden"
       v-if="finalizacaoCompra"
-      class="absolute m-auto left-0 right-0 z-50 bg-purple-200 w-2/4 h-2/3"
     >
-      <div>
-        <form class="w-full max-w-lg">
-          <div class="flex flex-wrap -mx-3 mb-6 ml-3 mt-10">
-            <button
-              class="absolute right-0 mr-4 mt-7 bg-yellow-500 hover:bg-yellow-800 text-black font-mono py-2 px-4 rounded-full"
-              @click="finalizacaoCompra = !finalizacaoCompra"
-            >
-              Fechar Fomulario
-            </button>
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 ml-3"
+      <div
+        v-if="finalizacaoCompra"
+        class="absolute m-auto left-0 right-0 z-30 bg-purple-200 w-2/4 h-2/3"
+      >
+        <div>
+          <form class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6 ml-3 mt-10">
+              <button
+                class="absolute right-0 mr-4 mt-7 bg-yellow-500 hover:bg-yellow-800 text-black font-mono py-2 px-4 rounded-full"
+                @click="finalizacaoCompra = !finalizacaoCompra"
               >
-                Primeiro Nome
-              </label>
-              <input
-                type="text"
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 ml-3 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                v-model="user.primeiro"
-                placeholder="Ex:Lucas"
-              />
-            </div>
-            <div class="w-full md:w-1/2 px-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-last-name"
-              >
-                Last Name
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                id="grid-last-name"
-                type="text"
-                v-model="user.ultimo"
-                placeholder="Ex:Antonete"
-              />
-            </div>
-
-            <div class="w-full md:w-3/5 mb-6 px-3">
-              <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Cartão de Crédito
+                Fechar Fomulario
+              </button>
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 ml-3"
+                >
+                  Primeiro Nome
                 </label>
                 <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  type="number"
-                  v-model="user.cartao"
-                  placeholder="Numero do Cartão"
+                  type="text"
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 ml-3 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  v-model="user.primeiro"
+                  placeholder="Ex: Jair"
                 />
               </div>
-            </div>
-            <div class="w-full md:w-2/5">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                Numero de Segurança
-              </label>
-              <input
-                type="number"
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                v-model="user.seguranca"
-                placeholder="Ex: 923"
-              />
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0 ml-3">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-zip"
-              >
-                Zip
-              </label>
-              <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 mr-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-zip"
-                type="text"
-                placeholder="90210"
-                v-model="user.zip"
-              />
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-state"
-              >
-                Estado
-              </label>
-              <div class="relative">
-                <select
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
-                  v-model="user.estado"
-                ></select>
+              <div class="w-full md:w-1/2 px-3">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-last-name"
+                >
+                  Last Name
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                  id="grid-last-name"
+                  type="text"
+                  v-model="user.ultimo"
+                  placeholder="Ex: Bolsonaro"
+                />
+              </div>
+              <div class="w-full md:w-3/5 mb-6 px-3">
+                <div class="w-full px-3">
+                  <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                    Cartão de Crédito
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    type="number"
+                    v-model="user.cartao"
+                    placeholder="Numero do Cartão"
+                  />
+                </div>
+              </div>
+              <div class="w-full md:w-2/5">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                  Numero de Segurança
+                </label>
+                <input
+                  type="number"
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  v-model="user.seguranca"
+                  placeholder="Ex: 923"
+                />
+              </div>
+              
+              <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0 ml-3">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-zip"
+                >
+                  Zip
+                </label>
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-green-500 rounded py-3 mr-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-zip"
+                  type="text"
+                  placeholder="90210"
+                  v-model="user.zip"
+                />
+              </div>
+              <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-state"
+                >
+                  Estado
+                </label>
+                <div class="relative">
+                  <select
+                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
+                    v-model="user.estado"
+                  ></select>
+                </div>
+              </div>
+              <div class="w-full md:w-1/4 px-3 mb-6 'md:mb-0">
+                <label
+                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  for="grid-state"
+                >
+                  cidade
+                </label>
+                <div class="relative">
+                  <select
+                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="grid-state"
+                    v-model="user.cidade"
+                  ></select>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
